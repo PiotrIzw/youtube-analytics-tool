069dfb59cc13f792ecb6cb374eb8e78c18765f67
PiotrIzw <piotr.izworski.0@gmail.com>
2020-12-26 21:53:02 +0100

    Initial commit

---
+++ src/main/java/com/company/youtubeanalyticstool/YoutubeAnalyticsToolApplication.java
@@ -0,0 +1,13 @@
+package com.company.youtubeanalyticstool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeAnalyticsToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoutubeAnalyticsToolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/company/youtubeanalyticstool/YoutubeAnalyticsToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.youtubeanalyticstool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeAnalyticsToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
